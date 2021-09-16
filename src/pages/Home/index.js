05fe2c007d3549c3bf21ca2c765923a19daf3be8
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-09-16 16:58:26 -0300

    create home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import { Grid, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({}));
+
+export default function Home() {
+  // const classes = useStyles();
+
+  return (
+    <Grid item container xs={12} direction="column" alignItems="center">
+      Corpo da página
+    </Grid>
+  );
+}
