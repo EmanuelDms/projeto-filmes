51624f4e6d13c00344beebfbecc548abdc63d18d
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-09-16 16:59:28 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Grid } from "@material-ui/core";
+import Routes from "./routes";
+
+// https://sujeitoprogramador.com/r-api/?api=filmes
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container>
+        <Routes />
+      </Grid>
+    </Box>
   );
 }
 
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+
+export default function Routes() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
+}
