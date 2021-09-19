c8c4d8e71e1f45ce55043fdd7c5204c2e5522e02
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-09-19 19:02:19 -0300

    create home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,36 @@
-import { Grid, makeStyles } from "@material-ui/core";
-
-const useStyles = makeStyles((theme) => ({}));
+import { useEffect, useState } from "react";
+import { Button, Grid, Typography } from "@material-ui/core";
+import useStyles from './styles';
+import { Link } from "react-router-dom";
+import movieRepository from '../../repositories/MovieRepository';
 
 export default function Home() {
-  // const classes = useStyles();
+  const [movies, setMovies] = useState([]);
+  const classes = useStyles();
+
+  useEffect(() => {
+    async function loadMovies() {
+      try {
+        const response = await movieRepository.get();
+        setMovies(response.data);
+      } catch (error) {
+        throw error;
+      }
+    }
+    loadMovies();
+  }, [])
 
   return (
-    <Grid item container xs={12} direction="column" alignItems="center">
-      Corpo da página
+    <Grid item container className={classes.moviesContainer}>
+      {movies.map((movie) => {
+        return <Grid item className={classes.movieItemContainer} key={movie.id}>
+          <Typography variant="h1">{movie.nome}</Typography>
+          <img src={movie.foto} alt={movie.nome} />
+          <Button variant="contained" fullWidth color="primary">
+            <Link to="/">Acessar</Link>
+          </Button>
+        </Grid>
+      })}
     </Grid>
   );
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,56 @@
+import { makeStyles } from "@material-ui/core";
+
+
+const useStyles = makeStyles((theme) => ({
+  moviesContainer: {
+    direction: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: theme.palette.background.paper,
+  },
+  movieItemContainer: {
+    margin: theme.spacing(2),
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    padding: theme.spacing(1.875),
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: theme.palette.background.default,
+    color: theme.palette.getContrastText(theme.palette.background.default),
+    '& h1': {
+      marginTop: 0,
+      textAlign: 'center'
+    },
+    '& strong': {
+      fontWeight: theme.typography.fontWeightBold
+    },
+    '& a': {
+      color: theme.palette.getContrastText(theme.palette.background.default)
+    },
+
+    '& img, &': {
+      [theme.breakpoints.down('sm')]: {
+        width: '100vw',
+        borderRadius: 0,
+      }
+    },
+    '& button': {
+      borderTopLeftRadius: 0,
+      borderTopRightRadius: 0,
+      [theme.breakpoints.down('sm')]: {
+        borderTopLeftRadius: theme.shape.borderRadius,
+        borderTopRightRadius: theme.shape.borderRadius,
+        width: '100vw',
+        marginTop: theme.spacing(2)
+      }
+    },
+    '& img': {
+      borderTopLeftRadius: theme.shape.borderRadius,
+      borderTopRightRadius: theme.shape.borderRadius
+    }
+  }
+}));
+
+export default useStyles;
