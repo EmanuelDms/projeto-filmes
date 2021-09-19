b5a61c96a1bb408feab8ef266556afdf338c3f28
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-09-19 19:03:49 -0300

    remove link api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { Box, Grid } from "@material-ui/core";
 import Routes from "./routes";
 
-// https://sujeitoprogramador.com/r-api/?api=filmes
-
 function App() {
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container>
