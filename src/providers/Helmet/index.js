7f3b59b48e3c29cf6d541ac8b5027ad0d6673a32
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-09-20 10:01:46 -0300

    change favicon and using helmet

---
+++ src/providers/Helmet/index.js
@@ -0,0 +1,14 @@
+import { useTheme } from '@material-ui/core';
+import { Helmet as Head, HelmetProvider as Provider } from 'react-helmet-async';
+
+export default function Helmet({ title }) {
+  const theme = useTheme();
+  return (
+    <Provider>
+      <Head>
+        <meta name="theme-color" content={theme.palette.common.black} />
+        <title>Filmes</title>
+      </Head>
+    </Provider>
+  )
+}
--- src/providers/index.js
+++ src/providers/index.js
@@ -1,11 +1,11 @@
+import Helmet from "./Helmet";
 import Theme from "./ThemeProvider";
 
 export default function Providers({ children }) {
   return (
-    <>
-      <Theme>
-        {children}
-      </Theme>
-    </>
+    <Theme>
+      <Helmet />
+      {children}
+    </Theme>
   )
 }
