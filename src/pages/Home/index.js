622132e184f395abfd8019ee2e531cda906783cd
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-09-21 16:14:40 -0300

    add loading

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,34 +1,49 @@
 import { useEffect, useState } from 'react';
-import { Button, Grid, Typography } from '@material-ui/core';
+import {
+  Button,
+  Grid,
+  CircularProgress,
+  Typography,
+  Box,
+} from '@material-ui/core';
 import useStyles from './styles';
 import { Link } from 'react-router-dom';
 import movieRepository from '../../repositories/MovieRepository';
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(0);
   const classes = useStyles();
 
   useEffect(() => {
     async function loadMovies() {
-      const response = await movieRepository.get();
-      setMovies(response.data);
+      setLoading(true);
+      const { data } = await movieRepository.get();
+      setLoading(false);
+      setMovies(data);
     }
     loadMovies();
   }, []);
 
   return (
     <Grid item container className={classes.moviesContainer}>
-      {movies.map((movie) => {
-        return (
-          <Grid item className={classes.movieItemContainer} key={movie.id}>
-            <Typography variant="h1">{movie.nome}</Typography>
-            <img src={movie.foto} alt={movie.nome} />
-            <Button variant="contained" fullWidth color="primary">
-              <Link to={`/filme/${movie.id}`}>Acessar</Link>
-            </Button>
-          </Grid>
-        );
-      })}
+      {!loading ? (
+        movies.map((movie) => {
+          return (
+            <Grid item className={classes.movieItemContainer} key={movie.id}>
+              <Typography variant="h1">{movie.nome}</Typography>
+              <img src={movie.foto} alt={movie.nome} />
+              <Button variant="contained" fullWidth color="primary">
+                <Link to={`/filme/${movie.id}`}>Acessar</Link>
+              </Button>
+            </Grid>
+          );
+        })
+      ) : (
+        <Box height="90vh" display="flex" alignItems="center">
+          <CircularProgress size={50} />
+        </Box>
+      )}
     </Grid>
   );
 }
